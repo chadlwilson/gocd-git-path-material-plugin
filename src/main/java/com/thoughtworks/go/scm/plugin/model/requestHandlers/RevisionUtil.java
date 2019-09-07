10d802e55d5757fe5aa2a4e9bcb1067d390ff56d
Chad Wilson <chadw@thoughtworks.com>
2019-09-07 18:29:33 +0800

    Add `LICENSE` - as Apache 2.0
    The code here appears to depend on some lift+shifted code from GoCD,
    or a plugin writing sample for GoCD, which would make it Apache 2.0 as
    well.

--- src/main/java/com/thoughtworks/go/scm/plugin/model/requestHandlers/RevisionUtil.java
+++ src/main/java/com/thoughtworks/go/scm/plugin/model/requestHandlers/RevisionUtil.java
@@ -4,16 +4,18 @@ import com.tw.go.plugin.model.Revision;
 import lombok.experimental.UtilityClass;
 
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @UtilityClass
 public class RevisionUtil {
+    private static final DateTimeFormatter ISO8601_FORMAT = new DateTimeFormatterBuilder().appendInstant(3).toFormatter();
 
     public Map<String, Object> toMap(Revision revision) {
         return Map.of(
                 "revision", revision.getRevision(),
-                "timestamp", DateTimeFormatter.ISO_INSTANT.format(revision.getTimestamp().toInstant()),
+                "timestamp", ISO8601_FORMAT.format(revision.getTimestamp().toInstant()),
                 "user", revision.getUser(),
                 "revisionComment", revision.getComment(),
                 "modifiedFiles",
