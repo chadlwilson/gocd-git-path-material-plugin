06179383eea6a4e8e5c5461aa518a9dbb3ef8deb
Chad Wilson <chadw@thoughtworks.com>
2019-10-13 19:09:01 +0800

    Make release script re-use the version from `gradle.properties`

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source token.sh
+source gradle.properties
 
 filename="linux-amd64-github-release.tar.bz2"
 os="linux"
@@ -19,26 +20,28 @@ fi
 
 GIT_HUB_RELEASE_BIN="./bin/${os}/amd64/github-release"
 
+echo "GitHub [${1}] of version ${version}"
+
 case "$1" in
   release)
     ${GIT_HUB_RELEASE_BIN} release \
       --user chadlwilson \
       --repo gocd-git-path-material-plugin \
-      --tag ${VERSION} \
-      --name ${VERSION} \
+      --tag ${version} \
+      --name ${version} \
       --pre-release;;
   upload)
     ${GIT_HUB_RELEASE_BIN} upload \
       --user chadlwilson \
       --repo gocd-git-path-material-plugin \
-      --tag ${VERSION} \
-      --name "gocd-git-path-material-plugin-$VERSION.jar" \
-      --file build/libs/gocd-git-path-material-plugin-${VERSION}.jar;;
+      --tag ${version} \
+      --name "gocd-git-path-material-plugin-$version.jar" \
+      --file build/libs/gocd-git-path-material-plugin-${version}.jar;;
   delete)
     ${GIT_HUB_RELEASE_BIN} delete \
       --user chadlwilson \
       --repo gocd-git-path-material-plugin \
-      --tag ${VERSION};;
+      --tag ${version};;
   *)
     echo "usage release.sh release | upload | delete"
 esac
